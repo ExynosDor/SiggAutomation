db3cb2b8a0e5a46f17be73e1eab8c3706e6603f5
Jiiva Durai <jeeva.durai@customerlabs.co>
2023-03-19 10:47:54 +0530

    Inital Commit

---
+++ requestPage.py
@@ -0,0 +1,77 @@
+import requests
+import re
+    
+def requestWebsite(url, cookie, userAgent):
+    headers = {
+        'Host': 'www.chegg.com',
+        "Accept-Encoding": "gzip, deflate, br",
+        'Accept-Language': 'en-US,en;q=0.5',
+        'cookie': cookie,
+        'upgrade-insecure-requests': '1',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'Upgrade-Insecure-Requests': '1',
+        'TE': 'trailers',
+        'user-agent': userAgent,
+        'referer': 'https://www.google.com/'
+    }
+    response = requests.get(url=url, headers=headers)
+    return response.text
+    
+def requestChapter(url, cookie, userAgent, html):
+    headers = {
+        'Host': 'www.chegg.com',
+        "Accept-Encoding": "gzip, deflate, br",
+        'Accept-Language': 'en-US,en;q=0.5',
+        'cookie': cookie,
+        'upgrade-insecure-requests': '1',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'Upgrade-Insecure-Requests': '1',
+        'TE': 'trailers',
+        'user-agent': userAgent,
+        'referer': 'https://www.google.com/'
+    }
+    token = re.search(r'"token":"(.+?)"', html).group(1)
+    chapter_id = str(re.search(r'\?id=(\d+).*?isbn', html).group(1))
+    isbn13 = str(re.search(r'"isbn13":"(\d+)"', html).group(1))
+    problemId = str(re.search(r'"problemId":"(\d+)"', html).group(1))
+    query = {
+        "query": {
+            "operationName": "getSolutionDetails",
+            "variables": {
+                "isbn13": isbn13,
+                "chapterId": chapter_id,
+                "problemId": problemId
+            }
+        },
+        "token": token
+    }
+    url = 'https://www.chegg.com/study/_ajax/persistquerygraphql'
+    response = requests.post(url=url, headers=headers, json=query, data=None)
+    return response.json()
+
+def requestEnhancedAnswer(url, cookie, userAgent, html):
+    headers = {
+        'Host': 'www.chegg.com',
+        "Accept-Encoding": "gzip, deflate, br",
+        'Accept-Language': 'en-US,en;q=0.5',
+        'cookie': cookie,
+        'upgrade-insecure-requests': '1',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'Upgrade-Insecure-Requests': '1',
+        'TE': 'trailers',
+        'user-agent': userAgent,
+        'referer': 'https://www.google.com/'
+    }
+    
